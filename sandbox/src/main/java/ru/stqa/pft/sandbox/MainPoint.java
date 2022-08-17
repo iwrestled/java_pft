13afecab5d1bdf8dd911b0c3937debfce494b793
Dmitry Limonov <iwrestled@gmail.com>
2022-08-17 09:55:35 +0300

    Fix задание №2

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MainPoint.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MainPoint.java
@@ -2,9 +2,12 @@ package ru.stqa.pft.sandbox;
 
 public class MainPoint {
     public static void main(String[] args) {
-        Point p1 = new Point(5,3);
-        Point p2 = new Point(3,-1);
+        Point p1 = new Point(5, 3);
+        Point p2 = new Point(3, -1);
 
-        System.out.println("Расстояние между точками = " + p1.distance(p1,p2));
+        System.out.println("Расстояние между точками = " + p1.distance(p2));
+
+
     }
+
 }
