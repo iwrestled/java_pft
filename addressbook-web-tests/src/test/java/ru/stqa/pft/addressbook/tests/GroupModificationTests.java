f0653aaec602bd3edf6c8e39df0fcfcca066af00
Dmitry Limonov <iwrestled@gmail.com>
2022-09-17 19:05:41 +0300

    Добавлены предусловия на наличие группы

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -7,6 +7,9 @@ public class GroupModificationTests extends TestBase {
     @Test
     public void testGroupModification() {
         app.getGroupHelper().gotoGroupPage();
+        if (! app.getGroupHelper().isThereAGroup()){
+            app.getGroupHelper().createGroup(new GroupData("AutoCreateGroup4", null, null));
+        }
         app.getGroupHelper().selectGroup();
         app.getGroupHelper().initGroupModification();
         app.getGroupHelper().fillGroupForm(new GroupData("ChangedName", "ChangedHeader", "Changed Footer"));
