cae71c6410642a8a842991544ac97ce83eff9399
Dmitry Limonov <iwrestled@gmail.com>
2022-12-14 09:39:30 +0300

    Fix дз18 manageUsers , managePage

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/NavigationHelper.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/NavigationHelper.java
@@ -9,9 +9,9 @@ public class NavigationHelper extends HelperBase{
     }
 
     public void managePage() {
-        click(By.cssSelector("a[href='/mantisbt-1.3.20/manage_overview_page.php']"));
+        click(By.cssSelector("a[href*='manage_overview_page.php']"));
     }
     public void manageUsers() {
-        click(By.cssSelector("a[href='/mantisbt-1.3.20/manage_user_page.php']"));
+        click(By.cssSelector("a[href*='manage_user_page.php']"));
     }
 }
