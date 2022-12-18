b53f21496740222f5eaffb4c8fd9a8613a006d38
Dmitry Limonov <iwrestled@gmail.com>
2022-12-18 19:06:39 +0300

    Lesson 9.5

---
+++ rest-sample/src/test/java/ru/stqa/pft/rest/RestAssuredTests.java
@@ -0,0 +1,53 @@
+package ru.stqa.pft.rest;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import com.google.gson.reflect.TypeToken;
+import com.jayway.restassured.RestAssured;
+import org.apache.http.client.fluent.Executor;
+import org.apache.http.client.fluent.Request;
+import org.apache.http.message.BasicNameValuePair;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.util.Set;
+
+import static org.testng.Assert.assertEquals;
+
+public class RestAssuredTests {
+
+    @BeforeClass
+    public void init(){
+        RestAssured.authentication = RestAssured.basic("02276e82280489b4fa0cd56b59abad4c","");
+    }
+
+    @Test
+    public void testCreateIssue() throws IOException {
+        Set<Issue> oldIssues = getIssues();
+        Issue newIssue = new Issue().withSubject("Test issue my 3").withDescription("Test description my 3");
+        int issueId = createIssue(newIssue);
+        Set<Issue> newIssues = getIssues();
+        oldIssues.add(newIssue.withId(issueId));
+        assertEquals(newIssues,oldIssues);
+
+    }
+    private Set<Issue> getIssues() throws IOException {
+//        String json = getExecutor().execute(Request.Get("https://bugify.stqa.ru/api/issues.json"))
+//                .returnContent().asString();
+        String json = RestAssured.get("https://bugify.stqa.ru/api/issues.json").asString();
+        JsonElement parsed = new JsonParser().parse(json);
+        JsonElement issues = parsed.getAsJsonObject().get("issues");
+        return new Gson().fromJson(issues,new TypeToken<Set<Issue>>(){}.getType());
+    }
+
+    private int createIssue(Issue newIssue) throws IOException {
+        String json = RestAssured.given()
+                .parameter("subject",newIssue.getSubject())
+                .parameter("description", newIssue.getDescription())
+                .post("https://bugify.stqa.ru/api/issues.json").asString();
+        JsonElement parsed = new JsonParser().parse(json);
+        return parsed.getAsJsonObject().get("issue_id").getAsInt();
+    }
+}
---
+++ rest-sample/src/test/java/ru/stqa/pft/rest/TestBase.java
@@ -0,0 +1,25 @@
+package ru.stqa.pft.rest;
+
+import org.testng.SkipException;
+
+import java.net.MalformedURLException;
+import java.rmi.RemoteException;
+
+public class TestBase {
+/*    public boolean isIssueOpen(int issueId) throws MalformedURLException, ServiceException, RemoteException {
+        Issue issue = app.soap().getIssueById(issueId);
+        String status = issue.getStatus();
+        return !(status.equals("resolved") || status.equals("closed"));
+    }
+
+    public void skipIfNotFixed(int issueId) throws MalformedURLException, ServiceException, RemoteException {
+        if (isIssueOpen(issueId)) {
+            throw new SkipException("Ignored because of issue " + issueId);
+        }
+    }
+
+ */
+
+
+
+}
