9e3d5096bc694b469cf4d6db14a9229cdf679de9
Dmitry Limonov <iwrestled@gmail.com>
2022-12-19 21:54:29 +0300

    Jenkins

---
+++ github-sample/src/test/java/ru/stqa/pft/github/GithubTests.java
@@ -0,0 +1,19 @@
+package ru.stqa.pft.github;
+
+import com.google.common.collect.ImmutableMap;
+import com.jcabi.github.*;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class GithubTests {
+
+    @Test
+    public void testCommits() throws IOException {
+        Github github = new RtGithub("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");
+        RepoCommits commits = github.repos().get(new Coordinates.Simple("iwrestled", "java_pft")).commits();
+        for (RepoCommit commit : commits.iterate(new ImmutableMap.Builder<String, String>().build())){
+            System.out.println(new RepoCommit.Smart(commit).message());
+        }
+    }
+}
